fecef2d0f914a169605a9f2c6d1742a1223744d1
Hector Vega <linnk99@gmail.com>
2020-01-11 20:18:26 -0500

    Excercise for going back and forward in pages added

--- E6_avanzar_retroceder.py
+++ E6_avanzar_retroceder.py
@@ -1,11 +1,30 @@
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from sleep import time
+from time import sleep
 
 class usando_unittest(unittest.TestCase):
     
     def setUp(self):
         self.driver = webdriver.Firefox(executable_path = "./geckodriver")
     
-    def test_next_or_previous_page()
+    def test_next_or_previous_page(self):
+        driver = self.driver
+        driver.get("https://www.google.com")
+        sleep(3)
+        driver.get("https://www.gmail.com")
+        sleep(3)
+        driver.get("https://www.youtube.com")
+        sleep(3)
+        driver.back()
+        sleep(3)
+        driver.back()
+        sleep(3)
+        driver.forward()
+        sleep(3)
+    
+    def tearDown(self):
+        self.driver.close()
+        
+if __name__ == "__main__":
+    unittest.main()
